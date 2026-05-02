673e22fb366b110342b146f8894cc5275315b740
Kancharla Srilakshmi <156530160+Srilakshmi-kancharla@users.noreply.github.com>
2026-05-02 11:44:12 +0530

    Li

--- Day-02-Searching-and-Sorting/LinearSearch.java
+++ Day-02-Searching-and-Sorting/LinearSearch.java
@@ -1,1 +1,30 @@
+import java.util.Scanner;
 
+class LinearSearch {
+ public static void main(String[] args) {
+  Scanner scan = new Scanner(System.in);
+  int n = scan.nextInt();
+  int arr[] = new int[n];
+  for(int i=0; i<=n-1; i++) {
+     arr[i] = scan.nextInt();
+  }
+  int searchElement = scan.nextInt();
+  for(int i=0; i<n; i++) {
+    if(arr[i] == searchElement) {
+      System.out.println("Element found at index: " + i);
+      return;
+    }
+  }
+  System.out.println("Element not found");
+ }
+}
+
+
+/*
+ n = 5
+ arr = [14, 5, 7, 8, 2]
+ searchElement = 7
+ OUTPUT:
+ Element found at index: 2
+ 
+*/ 
